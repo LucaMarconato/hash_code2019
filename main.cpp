cdb5507b9a7d1661157e2bab5569ef4024c4fa2c
Francesco Florian <francesco.florian94@gmail.com>
2019-02-28 20:25:44 +0100

    skeleton frame

--- main.cpp
+++ main.cpp
@@ -10,6 +10,21 @@
 
 using namespace std;
 
+typedef unsigned short d; //distance
+typedef int phi;        //photo index 
+typedef int ti;
+
+struct frame {
+    frame(picture* p1, picture p2){
+        
+    }
+    d niceness(frame* other) {
+        
+    }
+    vector<ti> tags; 
+    list<phi> images;
+}
+
 map<string, int> tags_dictionary;
 struct photo {
     bool vertical;
